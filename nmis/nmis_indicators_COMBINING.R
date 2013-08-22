512145ebffa048f7d7ad7bfd68aec2239e51453e
janghk <janghk@gmail.com>
2013-08-22 18:13:29 -0400

    use our script for proper escaping

--- nmis/nmis_indicators_COMBINING.R
+++ nmis/nmis_indicators_COMBINING.R
@@ -97,6 +97,6 @@ water_h_ed_ex <- merge(health_ed_ex, combined_774_lga_WATER, by="lga_id", all.x=
 #organizing
 #TODO: wtf is this line?
 #water_h_ed_ex <- water_h_ed_ex[,c(1:96,100:147,151:156,158:160,162:165,167:177)]
-selcol <- grep(".x$|.y$",colnames(water_h_ed_ex))
-water_h_ed_ex <- water_h_ed_ex[-c(selcol)]
-write.csv(water_h_ed_ex, "~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/nmis/data_774/All_774_LGA.csv", row.names=F)
+#selcol <- grep(".x$|.y$",colnames(water_h_ed_ex))
+#water_h_ed_ex <- water_h_ed_ex[-c(selcol)]
+write.csv(x_y_killa(water_h_ed_ex), "~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/nmis/data_774/All_774_LGA.csv", row.names=F)
