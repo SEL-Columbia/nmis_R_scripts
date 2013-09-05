f6da443f3e4ba90ba2bd7417c339539762e71195
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2013-09-05 14:47:22 -0400

    fixing the num_teachers_w_nce problem slightly more cleanly

--- nmis/nmis_indicators_education_facility_level_113.R
+++ nmis/nmis_indicators_education_facility_level_113.R
@@ -7,7 +7,8 @@ e_113 <- read.csv("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_proce
                   stringsAsFactors=F)
 
 ##### TO DO list:###
-e_661 <- read.csv("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/nmis/data_661/Education_661_NMIS_Facility.csv")
+e_661 <- read.csv("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/nmis/data_661/Education_661_NMIS_Facility.csv",
+                  nrow=1)
 
 ###113
 subnm <- names(e_661)[which(names(e_661) %in% names(e_113))]
@@ -44,12 +45,9 @@ ed$phcn_electricity <- e_113$power_grid_connection == T
 ###################
 
 ed$school_1kmplus_catchment_area <- e_113$km_to_catchment_area > 1
-ed$num_tchrs_with_nce <- rowSums(e_113[, c("tchrs_male_nce", "tchrs_female_nce")], na.rm=T) 
+ed$num_tchrs_w_nce <- as.numeric(e_113$num_tchrs_w_nce)
 ed$num_textbooks <- rowSums(e_113[,c("num_textbooks_english", "num_textbooks_math", 
                                      "num_textbooks_social_sci", "num_textbooks_pry_sci")], na.rm=T)
-
-# TODO: figure out a better place for this; this is an outlier-type statement
-ed$num_tchrs_with_nce <- ifelse(ed$num_tchrs_with_nce < ed$num_tchrs_total, ed$num_tchrs_with_nce, NA)
 
 ed$textbook_to_pupil_ratio <- ed$num_textbooks / e_113$num_students_total # NA & Inf introduced
 
