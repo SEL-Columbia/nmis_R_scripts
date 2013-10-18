266fd3adbd90dbecb50682a06d09db4f11a9b2f0
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2013-10-18 14:59:59 -0400

    commenting the rest of pavement.py

--- pavement.py
+++ pavement.py
@@ -99,17 +99,25 @@ def find_reads_and_writes(debug=False):
 
 
 @task
-def make_dependency_graph():
+def make_dependency_graph(filename='dependency_graph', format='pdf'):
+    """ Goes through all the R scripts in the current directory, finds all
+    \t\t\t files being read and written, creates a dependency graph, & outputs
+    \t\t\t it to filename.format (given that format is supported by graphviz)
+    \t\t\t Requires: graphviz to be installed, python module pydot. """
     import pydot
     deps = find_reads_and_writes()
+    # The pydot graph which we will re-encode the dependencies (deps) into
     graph = pydot.Dot(graph_type='digraph', rankdir='LR')
-    # create all nodes
+    # The nodes dict makes sure that each filename is added only once to graph
     nodes = {}
 
     def strip(s):
+        """ Takes a string like x/y/z/abc.def and returns abc.def """
         return s.split('/')[-1]
+
+    # Pass 1: add all nodes
     for k, v in deps.items():
-        k = strip(k)
+        k = strip(k)  # k is script name, stripped to just filename
         nodes[k] = pydot.Node(k, style='filled', fillcolor='red')
         graph.add_node(nodes[k])
         for datafile in v['inputs'] + v['outputs']:
@@ -118,7 +126,8 @@ def make_dependency_graph():
                 nodes[datafile] = pydot.Node(datafile, style='filled',
                                              fillcolor='#5F9F9F')
                 graph.add_node(nodes[datafile])
-    # create all edges
+
+    # Pass 2: add all edges
     for script, v in deps.items():
         script = strip(script)
         for infile in v['inputs']:
@@ -128,33 +137,46 @@ def make_dependency_graph():
             o = strip(outfile)
             graph.add_edge(pydot.Edge(nodes[script], nodes[o]))
     try:
-        graph.write('dependency_graph.pdf', format='pdf')
-    except pydot.InvocationException:
+        filename = "%s.%s" % (filename, format)
+        graph.write(filename)
+    except pydot.InvocationException:  # happens if graphviz isn't installed
         print "You need to install graphviz to create a dependency graph"
 
 
 @task
 def make_makefile(dryrun=False):
-    def fix_spaces(names):
+    """ Goes through all the R scripts in the current directory, finds all
+    \t\t\t files being read and written, and writes out makefile.
+    \t\t\t Given RSCRIPT that has infiles INF1, INF2 and outfiles OUTF1, OUTF2
+    \t\t\t the Makefile will have a rule like the below (with proper indents).
+
+    \t\t\t OUTF1 OUTF2: INF1 INF2 RSCRIPT
+    \t\t\t     $(R) RSCRIPT /dev/tty
+    """
+    def escape_spaces(names):
+        """ Replace space ( ) with escaped space(\ ) in a list of strings """
         return [name.replace(' ', '\ ') for name in names]
-    deps = find_reads_and_writes()
-    f = open('Makefile', 'w')
-    preamble = """R=R CMD BATCH --no-restore --slave
+    preamble = """\
+R=R CMD BATCH --no-restore --slave
 all:~/Dropbox/Nigeria/Nigeria\ 661\ Baseline\ Data\ Cleaning/\
 in_process_data/nmis/data_774/All_774_LGA.csv
 test:
 \tRscript tests/*.R > logs/test.log
 """
-    f.write(preamble)
-    for rscript, v in deps.items():
-        inputs = fix_spaces(v['inputs'])
-        outputs = fix_spaces(v['outputs'])
-        f.write(' '.join(outputs) + ": " + ' '.join(inputs + [rscript]) + '\n')
-        f.write('\t$(R) ' + rscript + ' /dev/tty\n')
-    f.close()
+
+    deps = find_reads_and_writes()
+    with open('Makefile', 'w') as f:
+        f.write(preamble)
+        for rscript, v in deps.items():
+            inputs = escape_spaces(v['inputs'])
+            outputs = escape_spaces(v['outputs'])
+            f.write(' '.join(outputs) + ": " +
+                    ' '.join(inputs + [rscript]) + '\n')
+            f.write('\t$(R) ' + rscript + ' /dev/tty\n')
 
 
 @task
 def make():
+    """ Create a new makefile and run make."""
     make_makefile()
     sh('make')
