037b34000545663ccfa3f0adc7f7845368828595
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2013-10-18 14:19:29 -0400

    pep8

--- pavement.py
+++ pavement.py
@@ -1,33 +1,43 @@
-from paver.easy import *
-from paver.path import *
+from paver.easy import task, path, sys, sh
 import re
-from os.path import expanduser, join
 import os
+import os.path
+
+""" This pavement.py uses the paver library to provide a series of tasks
+    that assist in creating a data pipeline from a set of R scripts within
+    the directory in which this pavement.py is placed.
+"""
+
+ROOT_DIR = os.path.join(os.path.expanduser('~'),
+                        'Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning')
 
 
-ROOT_DIR = join(expanduser('~'), 'Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning')
-
-
-def find_R_scripts(path='.'):
-    """ find all the R scripts in this folder"""
-    s = []
-    for root, dirs, files in os.walk(path):
+def find_R_scripts(base_directory='.'):
+    """ Return a list of all the R scripts in given base directory.
+    """
+    scripts = []
+    for root, dirs, files in os.walk(base_directory):
         for f in files:
             filename = os.path.join(root, f)
             if filename.endswith('.R') or filename.endswith('.r'):
-                s.append(filename)
-    #print s
-    return s
+                scripts.append(filename)
+    #print scripts
+    return scripts
+
 
 @task
 def find_reads_and_writes(debug=False):
     def file_exists_print_if_not(f, script):
-        fileexists = path(f.replace('~', expanduser('~'))).exists()
-        if not fileexists: sys.stderr.write("File does not exist: " + f + '; in script ' + script + '\n')
+        fileexists = path(f.replace('~', os.path.expanduser('~'))).exists()
+        if not fileexists:
+            sys.stderr.write("File does not exist: " + f +
+                             '; in script ' + script + '\n')
         return fileexists
+
     def only_existing_files(fnames, rscript):
-        return [fname for fname in fnames 
+        return [fname for fname in fnames
                 if file_exists_print_if_not(fname, rscript)]
+
     def read_excluding_comments(fname):
         f = open(fname, 'r')
         lines = f.readlines()
@@ -36,33 +46,38 @@ def find_reads_and_writes(debug=False):
         lines = [line for line in lines if not comment.match(line)]
         return '\n'.join(lines)
 
-    read_functions = ['read\.csv', 'formhubRead', 'save'] # look for first arguments
-    write_functions = ['write\.csv', 'load'] # look for second or later arugments
+    # Functions that read files (filename assumed to be first argument)
+    read_functions = ['read\.csv', 'formhubRead', 'load', 'readRDS']
+    # Functions that write files (filename assumed to be first argument)
+    write_functions = ['write\.csv', 'save', 'saveRDS']
+    # Functions that read+write files (filename in first argument is assumed
+    #   to be a read operation; second argument is assumed to be a write)
     rw_functions = ['file\.copy']
     rscripts = find_R_scripts()
     deps = {}
     for rscript in rscripts:
         text = read_excluding_comments(rscript)
         deps[rscript] = {'inputs': [], 'outputs': []}
-        for readf in read_functions:
-            candidate_in_deps = only_existing_files(re.findall(readf + r'''\(["']([^'"]*)["']''', text),
-                rscript)
-            deps[rscript]['inputs'] = deps[rscript]['inputs'] + candidate_in_deps
-        for writef in write_functions:
-            candidate_out_deps = only_existing_files(re.findall(writef + r'''\([^"]*["']([^"']*)["']''', text),
-                rscript)
-            deps[rscript]['outputs'] = deps[rscript]['outputs'] + candidate_out_deps
+        for rf in read_functions:
+            in_deps = re.findall(rf + r'''\(["']([^'"]*)["']''', text)
+            deps[rscript]['inputs'] = deps[rscript]['inputs'] + in_deps
+        for wf in write_functions:
+            out_deps = re.findall(wf + r'''\([^"]*["']([^"']*)["']''', text)
+            deps[rscript]['outputs'] = deps[rscript]['outputs'] + out_deps
         for rwf in rw_functions:
-            inouts = re.findall(rwf + r'''\(["']([^"']*)["'],\s*["']([^'"]*)["']''', text)
-            ins = only_existing_files([inf for inf,outf in inouts], rscript)
-            outs = only_existing_files([outf for inf,outf in inouts], rscript)
+            inouts = re.findall(rwf +
+                                r'''\(["']([^"']*)["'],\s*["']([^'"]*)["']''',
+                                text)
+            ins = [inf for inf, outf in inouts]
+            outs = [outf for inf, outf in inouts]
             deps[rscript]['inputs'] = deps[rscript]['inputs'] + ins
             deps[rscript]['outputs'] = deps[rscript]['outputs'] + outs
         if debug:
             print ">>> FILE: " + rscript
             print "***IN*** " + " ".join(deps[rscript]['inputs'])
             print "**OUT*** " + " ".join(deps[rscript]['outputs']) + '\n'
-    return deps 
+    return deps
+
 
 @task
 def make_dependency_graph():
@@ -71,6 +86,7 @@ def make_dependency_graph():
     graph = pydot.Dot(graph_type='digraph', rankdir='LR')
     # create all nodes
     nodes = {}
+
     def strip(s):
         return s.split('/')[-1]
     for k, v in deps.items():
@@ -80,9 +96,9 @@ def make_dependency_graph():
         for datafile in v['inputs'] + v['outputs']:
             datafile = strip(datafile)
             if datafile not in nodes.keys():
-                nodes[datafile] = pydot.Node(datafile, style='filled', fillcolor='#5F9F9F')
+                nodes[datafile] = pydot.Node(datafile, style='filled',
+                                             fillcolor='#5F9F9F')
                 graph.add_node(nodes[datafile])
-        
     # create all edges
     for script, v in deps.items():
         script = strip(script)
@@ -97,6 +113,7 @@ def make_dependency_graph():
     except pydot.InvocationException:
         print "You need to install graphviz to create a dependency graph"
 
+
 @task
 def make_makefile(dryrun=False):
     def fix_spaces(names):
@@ -104,20 +121,21 @@ def make_makefile(dryrun=False):
     deps = find_reads_and_writes()
     f = open('Makefile', 'w')
     preamble = """R=R CMD BATCH --no-restore --slave
-all:~/Dropbox/Nigeria/Nigeria\ 661\ Baseline\ Data\ Cleaning/in_process_data/nmis/data_774/All_774_LGA.csv
+all:~/Dropbox/Nigeria/Nigeria\ 661\ Baseline\ Data\ Cleaning/\
+in_process_data/nmis/data_774/All_774_LGA.csv
 test:
 \tRscript tests/*.R > logs/test.log
 """
     f.write(preamble)
-    for rscript,v in deps.items():
+    for rscript, v in deps.items():
         inputs = fix_spaces(v['inputs'])
         outputs = fix_spaces(v['outputs'])
         f.write(' '.join(outputs) + ": " + ' '.join(inputs + [rscript]) + '\n')
         f.write('\t$(R) ' + rscript + ' /dev/tty\n')
     f.close()
 
+
 @task
 def make():
     make_makefile()
     sh('make')
-
