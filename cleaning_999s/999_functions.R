37f5e3ed68b46358632733b2cc8de062562c2b51
Salah Chafik <salahchafik@gmail.com>
2013-11-05 17:17:28 -0500

    adding fucntion to 999 script to identify uuid + adding script to guide hard cleaning

--- cleaning_999s/999_functions.R
+++ cleaning_999s/999_functions.R
@@ -8,3 +8,12 @@ load_packages_with_install(c("data.table", "digest", "doBy"))
     set(dt, rows, cols, value)  
   }
 }
+
+#function for clarfying what is removed in above function
+cells_help_clean <- function(dt, cols, rows, value) {  
+  if (any(rows)) {   
+    print(dt$uuid[rows])
+    set(dt, rows, cols, value)  
+  }
+}
+
