5766d7bcb461e39884f4b97f010ff83c4d6f2a1e
zmyao88 <zm.yao88@gmail.com>
2013-11-08 01:20:50 -0500

    fixed json file parser and rerun the pipeline

--- facility_list_parser.py
+++ facility_list_parser.py
@@ -1,5 +1,9 @@
 import json
 import csv
+import os
+
+
+ROOT_DIR = os.path.join(os.path.expanduser('~'), 'Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/mop_up_matching_result')
 
 def nmis_id_json_parser(infile):
     """
@@ -12,10 +16,13 @@ def nmis_id_json_parser(infile):
         current = json.loads(element)
         nmis_uuid = current["long_id"]
         if current.has_key("matched"):
-            fcl_uuid = current["lga"]["long_id"]
-            fcl_facility_name = current["lga"]["facility_name"]
-            fcl_community = current["lga"]["community"]
-            fcl_ward = current["lga"]["ward"]
+            try:
+                fcl_uuid = current["lga"]["long_id"]
+                fcl_facility_name = current["lga"]["facility_name"]
+                fcl_community = current["lga"]["community"]
+                fcl_ward = current["lga"]["ward"]
+            except Exception, e:
+                print str(e)
         else:
             fcl_uuid = None
             fcl_facility_name = None
@@ -29,10 +36,32 @@ def nmis_id_json_parser(infile):
         id_list.append(obj)
     return id_list
 
-my_file = open('~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/mop_up_matching_result/jsondumps/nmis_edu.json')
-id_list = nmis_id_json_parser(my_file)
 
-out_file = open("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/mop_up_matching_result/baseline_matching_education.csv", "w")
-writer = csv.DictWriter(out_file, fieldnames = id_list[0].keys())
-writer.writeheader()
-writer.writerows(id_list)
+
+def run(input_file, output_file):
+    id_list = nmis_id_json_parser(input_file)
+    writer = csv.DictWriter(output_file, fieldnames = id_list[0].keys())
+    writer.writeheader()
+    writer.writerows(id_list)
+    input_file.close()
+    output_file.close()
+
+def my_file_path(relative_path, action='r'):
+    my_file = os.path.join(ROOT_DIR, relative_path)
+    return open(my_file, action)
+
+
+my_file = my_file_path('jsondumps/nmis_edu.json')
+out_file = my_file_path("baseline_matching_education.csv", "w")
+try:
+    run(my_file, out_file)
+except Exception,e:
+    print str(e)
+
+my_file = my_file_path('jsondumps/nmis_health.json')
+out_file = my_file_path("baseline_matching_health.csv", "w")
+try:
+    run(my_file, out_file)
+except Exception,e:
+    print str(e)
+
